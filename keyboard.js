9df18dbc3e1e97a9612d7da92ab3c1920d91b09b
Gboi123 <85195842+Gboi123@users.noreply.github.com>
2022-05-04 20:12:31 -0400

    Add files via upload

--- keyboard.js
+++ keyboard.js
@@ -1,13 +1,13 @@
 Status = "";
 keyboard_image = "";
-
+objects = []
 function preload(){
     keyboard_image = loadImage("keyboard.jpg");
 }
 
 function setup(){
     canvas = createCanvas(640,350);
-    canvas.position(315,200);
+    canvas.position(315,250);
     object_Detector = ml5.objectDetector('cocossd',modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
@@ -23,8 +23,21 @@ function gotResults(error,results){
         console.error(error);
     }
     console.log(results);
+    objects = results;
 }
 
 function draw(){
     image(keyboard_image,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x - 14, objects[i].y - 175);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x - 14, objects[i].y - 175, objects[i].width - 2326, objects[i].height - 2850);
+        }
+    }
 }
